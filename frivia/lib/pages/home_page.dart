db2b2e5d938200930041a8572fba1d27df98e83d
Troy Sandblom <troyzilla@gmail.com>
2022-05-01 17:50:10 -0700

    Added more text to the home screen.

--- frivia/lib/pages/home_page.dart
+++ frivia/lib/pages/home_page.dart
@@ -18,78 +18,110 @@ class _HomePageState extends State<HomePage> {
     _deviceHeight = MediaQuery.of(context).size.height;
     _deviceWidth = MediaQuery.of(context).size.width;
 
+    return _buildUI();
+  }
+
+  Widget _buildUI() {
     return Scaffold(
       body: SafeArea(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            const Text(
-              'Frivia',
-              style: TextStyle(
-                fontSize: 48,
-                //color: Colors.white,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            Text(
-              'Difficulty: ' + _difficulty,
-              style: const TextStyle(
-                fontSize: 24,
-                //color: Colors.white,
-              ),
-            ),
-            Slider(
-              value: _sliderValue.toDouble(),
-              min: 1.0,
-              max: 3.0,
-              onChanged: (double value) {
-                setState(() {
-                  _sliderValue = value.round();
-                  if (_sliderValue == 2) {
-                    _difficulty = 'medium';
-                  } else if (_sliderValue == 3) {
-                    _difficulty = 'hard';
-                  } else {
-                    _difficulty = 'easy';
-                  }
-                });
-                // if (v == 2.0) {
-                //   _difficulty = 'medium';
-                // } else if (v == 3.0) {
-                //   _difficulty = 'hard';
-                // } else {
-                //   _difficulty = 'easy';
-                // }
-              },
-            ),
-            MaterialButton(
-              minWidth: _deviceWidth! * 0.80,
-              height: _deviceHeight! * 0.10,
-              child: const Text(
-                'Start Game',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                  //color: Colors.white,
-                ),
-              ),
-              color: Colors.green,
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (c) {
-                      return GamePage(
-                        difficulty: _difficulty,
-                      );
-                    },
-                  ),
-                );
-              },
-            ),
+            _titleText(),
+            _difficultySlider(),
+            _startButton(),
           ],
         ),
       ),
+    );
+  }
+
+  Widget _titleText() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      mainAxisSize: MainAxisSize.max,
+      children: const [
+        Text(
+          'Frivia',
+          style: TextStyle(
+            fontSize: 48,
+            color: Colors.yellow,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Text(
+          'a free trivia game',
+          style: TextStyle(
+            fontSize: 24,
+            color: Colors.yellow,
+          ),
+        ),
+        Text(
+          "Select a difficulty level and then press 'Start Game'",
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _difficultySlider() {
+    return Column(
+      children: [
+        Text(
+          'Difficulty: ' + _difficulty,
+          style: const TextStyle(
+            fontSize: 24,
+            //color: Colors.white,
+          ),
+        ),
+        Slider(
+          value: _sliderValue.toDouble(),
+          min: 1.0,
+          max: 3.0,
+          onChanged: (double value) {
+            setState(() {
+              _sliderValue = value.round();
+              if (_sliderValue == 2) {
+                _difficulty = 'medium';
+              } else if (_sliderValue == 3) {
+                _difficulty = 'hard';
+              } else {
+                _difficulty = 'easy';
+              }
+            });
+          },
+        )
+      ],
+    );
+  }
+
+  Widget _startButton() {
+    return MaterialButton(
+      minWidth: _deviceWidth! * 0.80,
+      height: _deviceHeight! * 0.10,
+      child: const Text(
+        'Start Game',
+        style: TextStyle(
+          fontSize: 24,
+          fontWeight: FontWeight.bold,
+          //color: Colors.white,
+        ),
+      ),
+      color: Colors.green,
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (c) {
+              return GamePage(
+                difficulty: _difficulty,
+              );
+            },
+          ),
+        );
+      },
     );
   }
 }
